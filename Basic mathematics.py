7f6a53e2bd056bcac6fa957e7f68df6d115e726b
Anurag28T <thakareanurag28@gmail.com>
2025-06-08 11:16:36 -0500

    Add files via upload

---
+++ Basic mathematics.py
@@ -0,0 +1,22 @@
+#Task 1
+
+num1=5
+num2=10
+add=num1+num2
+print('addition is ',add)
+
+sub=num1-num2
+print('subtraction is',sub)
+
+mul=num1*num2
+print('multiplication is',mul)
+
+div=num1/num2
+print('division is',div)
+
+ #Task 2
+
+first_name=input("Enter your first name:")
+last_name=input("Enter your Last name:  ")
+full_name= first_name + ""+last_name
+print("Hello," + full_name + "!Welcome To Python programming.")
